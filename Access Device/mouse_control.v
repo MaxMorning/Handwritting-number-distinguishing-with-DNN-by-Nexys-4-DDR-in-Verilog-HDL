1466f880ff9e62c12cd470f2840b56e81005d6f3
MaxMorning <hml0814@163.com>
2020-12-22 11:00:07 +0800

    Add some code for accessing device
    Add some code for accessing device.
    Set gitignore.

---
+++ Access Device/mouse_control.v
@@ -0,0 +1,84 @@
+module mouse_control(
+    input iRst_n,
+    inout clkBus,
+    inout dataBus,
+    output reg [15:0] oLights
+);
+
+    reg [10:0] data;
+    reg [3:0] cnt;
+    reg [1:0] byteCnt;
+    reg clkBusSendEna;
+    reg dataBusSendEna;
+
+    reg clkSendBit;
+    reg clkReceiveBit;
+    assign clkBus = clkBusSendEna ? clkSendBit : 1'bz;
+    assign clkReceiveBit = clkBus;
+
+    reg dataSendBit;
+    reg dataReceiveBit;
+    reg [3:0] cursorPos;
+    reg eccError;
+    assign dataBus = dataBusSendEna ? dataSendBit : 1'bz;
+    assign dataReceiveBit = dataBus;
+
+    reg ecc;
+    always @ (negedge clkBus or negedge iRst_n or posedge eccError) begin
+        if (!iRst_n) begin
+            ecc <= 1'b0;
+            cnt <= 10;
+            byteCnt <= 0;
+            clkBusSendEna <= 0;
+            dataBusSendEna <= 0;
+            oLights <= 16'b0000000001000000;
+            cursorPos <= 6;
+            eccError <= 0;
+        end
+        else if (eccError) begin
+            
+        end
+        else begin
+            data[cnt] <= dataBus;
+            ecc = dataBus ? ~ecc : ecc;
+            if (cnt == 0) begin // receive 11 bit
+                cnt <= 10;
+
+                // check valid
+                if (!ecc) begin
+                    // error, communicate with slave
+                    clkBusSendEna <= 1'b1;
+                    dataSendBit <= 1'b0;
+                    eccError <= 1'b1;
+                end
+                else begin
+                    // correct , process
+                    case (byteCnt)
+                        0: // first byte
+                            begin
+                                oLights[15] <= data[9];
+                                oLights[14] <= data[8];
+                                byteCnt <= 1;
+                            end
+                        1: // x shift
+                            begin
+                                byteCnt <= 2;
+                                if (data[9] == 0)
+                                    cursorPos <= cursorPos + data[8:7];
+                                else
+                                    cursorPos <= cursorPos - data[8:7];
+                                oLights[13:0] = 14'b0;
+                                oLights[cursorPos] = 1'b1;
+                            end
+                        2: // y shift
+                            begin
+                                byteCnt <= 0;
+                            end
+                    endcase
+                end
+            end
+            else 
+                cnt <= cnt - 1;
+        end
+    end
+endmodule
---
+++ Access Device/vga_test.v
@@ -0,0 +1,69 @@
+module vga_test(
+    input iBusClk, // Bus Clock
+    input iRstN, // Async reset signal
+    output reg [3:0] oRed, // red signal
+    output reg [3:0] oGreen, // green signal
+    output reg [3:0] oBlue, // blue signal
+    output oHs, // Hori sync
+    output oVs // Vert sync
+);
+
+    // 800 * 600
+    parameter   C_H_SYNC_PULSE   = 128, 
+                C_H_BACK_PORCH   = 88,
+                C_H_ACTIVE_TIME  = 800,
+                C_H_FRONT_PORCH  = 40,
+                C_H_LINE_PERIOD  = 1056;
+    
+    parameter   C_V_SYNC_PULSE   = 4, 
+                C_V_BACK_PORCH   = 23,
+                C_V_ACTIVE_TIME  = 600,
+                C_V_FRONT_PORCH  = 1,
+                C_V_FRAME_PERIOD = 623;
+
+    reg [10:0] hCnt; // Hori Counter
+    reg [10:0] vCnt; // Vert Counter
+    reg clkVga; // VGA clock, 40MHz
+    wire isActive;
+    reg [11:0] image [799:0][599:0];
+
+
+    // Hori
+    always @ (posedge clkVga or negedge iRstN) begin
+        if (!iRstN || hCnt == C_H_LINE_PERIOD - 1)
+            hCnt <= 11'd0;
+        else
+            hCnt <= hCnt + 1;
+    end
+
+    assign oHs = (hCnt < C_H_SYNC_PULSE) ? 1'b0 : 1'b1;
+
+
+    //Vert
+    always @ (posedge clkVga or negedge iRstN) begin
+        if (!iRstN || vCnt == C_V_FRAME_PERIOD - 1)
+            vCnt <= 11'd0;
+        else
+            vCnt <= vCnt + 1;
+    end
+
+    assign oVs = (vCnt < C_V_SYNC_PULSE) ? 1'b0 : 1'b1;
+
+    assign isActive =   (hCnt >= (C_H_SYNC_PULSE + C_H_BACK_PORCH                  ))  &&
+                        (hCnt <= (C_H_SYNC_PULSE + C_H_BACK_PORCH + C_H_ACTIVE_TIME))  && 
+                        (vCnt >= (C_V_SYNC_PULSE + C_V_BACK_PORCH                  ))  &&
+                        (vCnt <= (C_V_SYNC_PULSE + C_V_BACK_PORCH + C_V_ACTIVE_TIME))  ;
+
+    always @ (posedge clkVga or negedge iRstN) begin
+        if (!iRstN) begin
+            oRed <= 4'b0000;
+            oGreen <= 4'b0000;
+            oBlue <= 4'b0000;
+        end
+        else if (isActive) begin
+            oRed <= image[hCnt][vCnt][3:0];
+            oGreen <= image[hCnt][vCnt][7:4];
+            oBlue <= image[hCnt][vCnt][11:8];
+        end
+    end
+endmodule
